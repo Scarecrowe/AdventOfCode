b6219b666b1754096acce911d1669087cc50dcc0
Scarecrowe <scarecroweman@gmail.com>
2023-08-28 16:56:03 +0100

    AOC-2 updated the command argument format

--- AdventOfCode.Runner/CommandArguments.cs
+++ AdventOfCode.Runner/CommandArguments.cs
@@ -6,17 +6,29 @@
     {
         public CommandArguments(string[] args)
         {
-            if (args.Length != 4)
+            for (int i = 0; i < args.Length; i++)
             {
-                Console.WriteLine("Invalid command arguments length");
-                this.Valid = false;
-                return;
+                switch (args[i])
+                {
+                    case "-year":
+                        this.Year = this.ParseArgument(args, i);
+                        i++;
+                        break;
+                    case "-day":
+                        this.Day = this.ParseArgument(args, i);
+                        i++;
+                        break;
+                    case "-batch":
+                        this.BatchCount = this.ParseArgument(args, i);
+                        i++;
+                        break;
+                }
             }
 
-            this.Valid = true;
-            this.Year = args[1].ToInt();
-            this.Day = args[2].ToInt();
-            this.BatchCount = args[3].ToInt();
+            if (this.BatchCount == 0)
+            {
+                this.BatchCount = 1;
+            }
         }
 
         public int Year { get; }
@@ -25,6 +37,26 @@
 
         public int BatchCount { get; }
 
-        public bool Valid { get; }
+        public bool Valid
+        {
+            get
+            {
+                return !((this.Year < 2015 || this.Year > 2022)
+                    || (this.Day < 1 || this.Day > 25));
+            }
+        }
+
+        private int ParseArgument(string[] args, int i)
+        {
+            int value = 0;
+
+            if (i + 1 >= args.Length
+            || !int.TryParse(args[i + 1], out value))
+            {
+                throw new ArgumentException($"Invalid ${args[i].Replace("-")} command argument");
+            }
+
+            return value;
+        }
     }
 }
