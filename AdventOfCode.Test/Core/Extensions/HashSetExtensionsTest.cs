e4a6b2280b5fad911408d839a55a107977287839
Scarecrowe <scarecroweman@gmail.com>
2023-08-29 20:55:50 +0100

    AOC-7 added unit tests covering hash extensions

--- AdventOfCode.Test/Core/Extensions/HashSetExtensionsTest.cs
+++ AdventOfCode.Test/Core/Extensions/HashSetExtensionsTest.cs
@@ -1,6 +1,7 @@
 ﻿namespace AdventOfCode.Test.Core
 {
-    using AdventOfCode.Core;
+    using AdventOfCode.Core.Extensions;
+    using FluentAssertions;
     using NUnit.Framework;
 
     public class HashSetExtensionsTestBase
@@ -14,5 +15,35 @@
     [TestFixture]
     public class HashSetExtensionsTest : HashSetExtensionsTestBase
     {
+        public class AddRange : HashSetExtensionsTestBase
+        {
+            [Test]
+            public void AddRange_ShouldAddAllValuesToHashSet()
+            {
+                HashSet<int> hashSet = new() { 1, 2, 3 };
+                HashSet<int> valuesToAdd = new() { 3, 4, 5 };
+
+                hashSet.AddRange(valuesToAdd);
+
+                hashSet.ElementAt(0).Should().Be(1);
+                hashSet.ElementAt(1).Should().Be(2);
+                hashSet.ElementAt(2).Should().Be(3);
+                hashSet.ElementAt(3).Should().Be(4);
+                hashSet.ElementAt(4).Should().Be(5);
+            }
+
+            [Test]
+            public void AddRange_WhenEmptyValuesToAdd_ShouldNotChangeHashSet()
+            {
+                HashSet<int> hashSet = new HashSet<int> { 1, 2, 3 };
+                HashSet<int> valuesToAdd = new HashSet<int>();
+
+                hashSet.AddRange(valuesToAdd);
+
+                hashSet.ElementAt(0).Should().Be(1);
+                hashSet.ElementAt(1).Should().Be(2);
+                hashSet.ElementAt(2).Should().Be(3);
+            }
+        }
     }
 }
