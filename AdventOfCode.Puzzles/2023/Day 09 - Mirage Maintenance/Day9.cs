d41240baebf9dd72e038bbe68898582bc294884a
Scarecrowe <scarecroweman@gmail.com>
2023-12-09 06:29:28 +0000

    AOC-24 Puzzle - 2023 - Day 9: Mirage Maintenance

---
+++ AdventOfCode.Puzzles/2023/Day 09 - Mirage Maintenance/Day9.cs
@@ -0,0 +1,18 @@
+﻿namespace AdventOfCode.Puzzles._2023.Days
+{
+    using AdventOfCode.Core;
+    using AdventOfCode.Puzzles._2023.Day_09___Mirage_Maintenance;
+
+    public class Day9 : Puzzle, IPuzzle
+    {
+        public Day9(string file)
+        {
+            this.DayTitle = "Mirage Maintenance";
+            this.GetPuzzleData(file);
+        }
+
+        public string Silver() => $"{new MirageMaintenance(this.Input).End()}";
+
+        public string Gold() => $"{new MirageMaintenance(this.Input).Beginning()}";
+    }
+}
---
+++ AdventOfCode.Puzzles/2023/Day 09 - Mirage Maintenance/MirageMaintenance.cs
@@ -0,0 +1,27 @@
+﻿namespace AdventOfCode.Puzzles._2023.Day_09___Mirage_Maintenance
+{
+    using AdventOfCode.Core.Extensions;
+
+    public class MirageMaintenance
+    {
+        public MirageMaintenance(string[] input)
+            => this.Report = input.Select(x => x.Split(" ").ToInt()).ToList();
+
+        public List<int[]> Report { get; }
+
+        public int End()
+            => this.ExtrapolateHistory((history, result) => history[^1] + result);
+
+        public int Beginning()
+            => this.ExtrapolateHistory((history, result) => history[0] - result);
+
+        private static int Predict(int[] history, Func<int[], int, int> func)
+            => history.Length == 0 ? 0 : func(history, Predict(Diff(history), func));
+
+        private static int[] Diff(int[] history)
+            => history[0..^1].Select((x, i) => history[i + 1] - x).ToArray();
+
+        private int ExtrapolateHistory(Func<int[], int, int> func)
+            => this.Report.Aggregate(0, (total, history) => total += Predict(history, func));
+    }
+}
